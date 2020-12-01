344a4e9496d38b7ea3177ec39908f63b6861326b
Brais Caloto <braiscaloto92@gmail.com>
2020-12-01 12:22:54 +0100

    redux iniciado

--- diary-app/src/DiaryApp.js
+++ diary-app/src/DiaryApp.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { AppRouter } from './routers/AppRouter';
+import { Provider } from 'react-redux';
+import { store } from './store/store';
 
 export const DiaryApp = () => {
 	return (
 		<div>
-			<AppRouter />
+			<Provider store={store}>
+				<AppRouter />
+			</Provider>
 		</div>
 	);
 };
