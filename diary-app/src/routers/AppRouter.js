a972a20c1af8ea27838f3468d594483166245121
Brais Caloto <braiscaloto92@gmail.com>
2020-12-01 16:54:21 +0100

    use effect mantener autenticación

--- diary-app/src/routers/AppRouter.js
+++ diary-app/src/routers/AppRouter.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {
 	Redirect,
 	Route,
@@ -7,8 +7,20 @@ import {
 } from 'react-router-dom';
 import { DiaryScreen } from '../components/diary/DiaryScreen';
 import { AuthRouter } from './AuthRouter';
+import { firebase } from '../firebase/firebase-config';
+import { useDispatch } from 'react-redux';
+import { login } from '../actions/auth';
 
 export const AppRouter = () => {
+	const dispatch = useDispatch();
+	useEffect(() => {
+		firebase.auth().onAuthStateChanged((user) => {
+			if (user?.uid) {
+				dispatch(login(user.uid, user.displayName));
+			}
+		});
+	}, [dispatch]);
+
 	return (
 		<Router>
 			<div>
